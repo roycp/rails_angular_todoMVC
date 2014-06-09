97350dd76f31e6be53addb33687ac12e9d35d02b
Roy Cruz <royalbertoc@gmail.com>
2014-06-08 21:18:29 -0600

    eliminate comments

--- app/assets/javascripts/angular/services/todoStorage.js
+++ app/assets/javascripts/angular/services/todoStorage.js
@@ -26,15 +26,4 @@ angular.module('todomvc')
             method: 'DELETE'
         }
     });
-		// var STORAGE_ID = 'todos-angularjs';
-
-		// return {
-		// 	get: function () {
-		// 		return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-		// 	},
-
-		// 	put: function (todos) {
-		// 		localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		// 	}
-		// };
-	});
+});
